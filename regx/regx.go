a47a87741dba91bf2473ee7afb7db5b72c2d7cc8
britannic <github@empirecreekcircle.com>
2016-06-04 18:39:15 -0700

    regx.go updates

--- regx/regx.go
+++ regx/regx.go
@@ -11,6 +11,28 @@ import (
 	"regexp"
 )
 
+// Objects is a struct of *OBJ populated with precompiled regex objects
+var Objects = &OBJ{
+	CMNT: regexp.MustCompile(`^(?:[\/*]+)(.*?)(?:[*\/]+)$`),
+	DESC: regexp.MustCompile(`^(?:description)+\s"?([^"]+)?"?$`),
+	DSBL: regexp.MustCompile(`^(?:disabled)+\s([\S]+)$`),
+	FLIP: regexp.MustCompile(`^(?:address=[/][.]{0,1}.*[/])(.*)$`),
+	FQDN: regexp.MustCompile(`\b((?:(?:[^.-/]{0,1})[a-zA-Z0-9-_]{1,63}[-]{0,1}[.]{1})+(?:[a-zA-Z]{2,63}))\b`),
+	HOST: regexp.MustCompile(`^(?:address=[/][.]{0,1})(.*)(?:[/].*)$`),
+	HTTP: regexp.MustCompile(`(?:^(?:http|https){1}:)(?:\/|%2f){1,2}(.*)`),
+	IPBH: regexp.MustCompile(`^(?:dns-redirect-ip)+\s([\S]+)$`),
+	LBRC: regexp.MustCompile(`[{]`),
+	// LEAF: regexp.MustCompile(`^(source)+\s([\S]+)\s[{]{1}$`),
+	LEAF: regexp.MustCompile(`^([\S]+)+\s([\S]+)\s[{]{1}$`),
+	MISC: regexp.MustCompile(`^([\w-]+)$`),
+	MLTI: regexp.MustCompile(`^((?:include|exclude)+)\s([\S]+)$`),
+	MPTY: regexp.MustCompile(`^$`),
+	NAME: regexp.MustCompile(`^([\w-]+)\s["']{0,1}(.*?)["']{0,1}$`),
+	NODE: regexp.MustCompile(`^([\w-]+)\s[{]{1}$`),
+	RBRC: regexp.MustCompile(`[}]`),
+	SUFX: regexp.MustCompile(`(?:#.*|\{.*|[/[].*)\z`),
+}
+
 // OBJ is a struct of regex precompiled objects
 type OBJ struct {
 	CMNT, DESC, DSBL, FLIP, FQDN, HOST, HTTP, IPBH, LEAF, LBRC, MISC, MLTI, MPTY, NAME, NODE, RBRC, SUFX *regexp.Regexp
@@ -69,26 +91,4 @@ func (rx *OBJ) String() (result string) {
 		result += fmt.Sprintf("%v: %v\n", v.Type().Field(i).Name, v.Field(i).Interface())
 	}
 	return result
-}
-
-// Objects is a struct of *OBJ populated with precompiled regex objects
-var Objects = &OBJ{
-	CMNT: regexp.MustCompile(`^(?:[\/*]+)(.*?)(?:[*\/]+)$`),
-	DESC: regexp.MustCompile(`^(?:description)+\s"?([^"]+)?"?$`),
-	DSBL: regexp.MustCompile(`^(?:disabled)+\s([\S]+)$`),
-	FLIP: regexp.MustCompile(`^(?:address=[/][.]{0,1}.*[/])(.*)$`),
-	FQDN: regexp.MustCompile(`\b((?:(?:[^.-/]{0,1})[a-zA-Z0-9-_]{1,63}[-]{0,1}[.]{1})+(?:[a-zA-Z]{2,63}))\b`),
-	HOST: regexp.MustCompile(`^(?:address=[/][.]{0,1})(.*)(?:[/].*)$`),
-	HTTP: regexp.MustCompile(`(?:^(?:http|https){1}:)(?:\/|%2f){1,2}(.*)`),
-	IPBH: regexp.MustCompile(`^(?:dns-redirect-ip)+\s([\S]+)$`),
-	LBRC: regexp.MustCompile(`[{]`),
-	// LEAF: regexp.MustCompile(`^(source)+\s([\S]+)\s[{]{1}$`),
-	LEAF: regexp.MustCompile(`^([\S]+)+\s([\S]+)\s[{]{1}$`),
-	MISC: regexp.MustCompile(`^([\w-]+)$`),
-	MLTI: regexp.MustCompile(`^((?:include|exclude)+)\s([\S]+)$`),
-	MPTY: regexp.MustCompile(`^$`),
-	NAME: regexp.MustCompile(`^([\w-]+)\s["']{0,1}(.*?)["']{0,1}$`),
-	NODE: regexp.MustCompile(`^([\w-]+)\s[{]{1}$`),
-	RBRC: regexp.MustCompile(`[}]`),
-	SUFX: regexp.MustCompile(`(?:#.*|\{.*|[/[].*)\z`),
 }
